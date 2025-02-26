ca12d3599ea98803500e0610e1a20064b4e023f0
Chirag Soni <csoni0693@gmail.com>
2025-02-26 10:05:28 +0530

    vaishnav-library

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -17,18 +17,28 @@ const Home = ({contactRef}) => {
   };
 
   return (
-    <div className="relative flex flex-col items-center justify-between w-full h-full p-6 px-40 mt-32 font-bold md:flex-row">
+    <div className="relative flex flex-col items-center justify-between w-full h-full p-6 px-6 sm:px-12 md:px-16 lg:px-40 mt-32 font-bold md:flex-row">
       <div className="z-10 space-y-4 text-center text-white md:text-left md:w-1/2">
-        <h2 className="text-[#08D665] text-xl md:text-2xl">HELLO!</h2>
-        <h1 className={`text-3xl md:text-4xl font-bold uppercase text-white}`}>
-          <ReactTyped strings={["Track books, members, and issues seamlessly","Digitalize your library management","Efficient library solutions at your fingertips"]} typeSpeed={50} loop backSpeed={30} />
+        <h2 className="text-[#08D665] text-xl sm:text-2xl md:text-3xl">HELLO!</h2>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold uppercase text-white">
+          <ReactTyped
+            strings={["Track books, members, and issues seamlessly", "Digitalize your library management", "Efficient library solutions at your fingertips"]}
+            typeSpeed={50}
+            loop
+            backSpeed={30}
+          />
         </h1>
         
-        <p className={`text-sm md:text-base max-w-lg mx-auto md:mx-0 text-white `}>
-        A library management system designed to simplify the process of managing books, members, and book lending. Keep track of all your inventory, monitor book availability, manage members, and easily record issued and returned books.
+        <p className="text-sm sm:text-base md:text-lg max-w-lg mx-auto md:mx-0 text-white">
+          A library management system designed to simplify the process of managing books, members, and book lending. Keep track of all your inventory, monitor book availability, manage members, and easily record issued and returned books.
         </p>
         
-        <button ref={contactRef} onClick={()=>scrollToSection(contactRef)} className="text-white bg-[#08D665] p-3 rounded-md mt-4">Contact Me</button>
+        <button 
+          ref={contactRef} 
+          onClick={() => scrollToSection(contactRef)} 
+          className="text-white bg-[#08D665] p-3 rounded-md mt-4 sm:mt-6 md:mt-8 text-sm sm:text-base">
+          Contact Me
+        </button>
       </div>
       
       <div className="z-10 flex justify-center mt-8 md:mt-0">
